f174c82732f6ec333f3e2a957486c717b516ce4d
thecyberjerry <himanshusharma2719@gmail.com>
2024-06-25 17:46:19 +0530

    new middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 export { withAuth } from "next-auth/middleware";
 export { default } from "next-auth/middleware";
 export const config = { matcher: ["/account", "/account/((?!.*\\.).*)"] }
-=======
-export { default } from "next-auth/middleware"
-
-export const config = { matcher: ["/account"] }
->>>>>>> e12fb7ec44b5bbd069c2f8d0530a525c2acaa564
