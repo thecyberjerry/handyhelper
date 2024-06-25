376f84223a13cfa98e80f5209b92fef420bf2563
thecyberjerry <himanshusharma2719@gmail.com>
2024-06-25 20:42:49 +0530

    final

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,3 @@
 export { default } from "next-auth/middleware"
 //currently this middleware is not working as expected
-<<<<<<< HEAD
 export const config = { matcher: ["/dashboard/:path*"] }
-=======
-export const config = { matcher: ["/dashboard/:path*"] }
->>>>>>> 5745c3a17117f9b1fd71c8faec645773950c2e4e
