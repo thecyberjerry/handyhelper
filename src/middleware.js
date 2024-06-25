8aeeb460b9895a34759fb92239c706a7a36ce2c4
Himanshu <72544756+thecyberjerry@users.noreply.github.com>
2024-06-25 16:48:16 +0530

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,3 @@
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/account","/account/((?!.*\\.).*)"] }
+export const config = { matcher: ["/account","/account/changepassword","/account/changeemail","/account/shopimages","/account/updateinfo"] }
