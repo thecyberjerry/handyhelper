2b5e7ccfd69171317a9a45f91847e78ec1dc328c
thecyberjerry <himanshusharma2719@gmail.com>
2024-06-26 10:35:37 +0530

    middleware update

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,3 @@
 export { default } from "next-auth/middleware"
-//currently this middleware is not working as expected
-const secret = process.env.NEXTAUTH_SECRET
-console.log(secret);
 
 export const config = { matcher: ["/account/:path*"] }
